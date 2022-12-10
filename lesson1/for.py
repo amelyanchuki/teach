491bdad33e9e8ac2505b60fa4fcb308d45bf7951
unknown <amelyanchuk.i@gmail.com>
2022-12-10 12:36:20 +0300

    update for and while

--- lesson1/for.py
+++ lesson1/for.py
@@ -21,3 +21,29 @@ def arethmetic():
         d += 1
 
     print(s / d)
+
+def any():
+    a = int(input()) #50
+    b = []
+
+    for i in range(1, a + 1):
+        if len(b) >= a:
+            break
+        b += [i] * i
+    print(*b[:a])
+
+
+a = (int(i) for i in input().split())
+b = int(input())
+c = 0
+s = []
+
+for i in a:
+    if i == b:
+        s.append(c)
+    c += 1
+
+if s == []:
+    print("Отсутствует")
+else:
+    print(*s)
--- lesson1/while.py
+++ lesson1/while.py
@@ -1,12 +1,24 @@
-x = int(input("введите число\t"))
-while x < 100:
-    y = 1 
-    x += y
-    if x % 2 != 0:
-        continue
-    if x == 100:
-        print("x больше 100")
-        break
-    print(x)
-else:
-    print(f"Х больше 100 или ввели строку длинной {len(x)} символа")
+def chet():
+    x = int(input("введите число\t"))
+    while x < 100:
+        y = 1 
+        x += y
+        if x % 2 != 0:
+            continue
+        if x == 100:
+            print("x больше 100")
+            break
+        print(x)
+    else:
+        print(f"Х больше 100 или ввели строку длинной {len(x)} символа")
+
+def summ_multipl():
+    b = 0
+    c = 0
+    while True:
+        a = int(input())
+        c += a * a
+        b += a
+        if b == 0:
+            print(c)
+            break
