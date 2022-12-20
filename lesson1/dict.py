81289bfe93571382f1e8c458ce54a1f9ca8b9fcd
unknown <amelyanchuk.i@gmail.com>
2022-12-20 23:31:44 +0300

    update dict

--- lesson1/dict.py
+++ lesson1/dict.py
@@ -1,7 +1,5 @@
 a = {"fng": 35261, "awf": 12351, "ilidan": 125167}
 
-
-
 def vivers():
     c = {}
     for i, s in a.items():
@@ -12,5 +10,43 @@ def main():
     for key, val in vivers().items():
         print(f"сейчас стримит {key} и его смотрит {val} зрителей")
 
+d = {}
 
-main()
+def update_directory(d, key, value):
+    if key in d:
+        d[key] += [value]
+    elif key not in d:
+        if 2*key in d:
+            d[2*key] += [value]
+        elif 2*key not in d:
+            d[2*key] = [value]
+
+c = {}
+b = input().lower().split()
+
+for i in b:
+    if i in c:
+        c[i] += 1
+    elif i not in c:
+        c[i] = 1
+
+for key, val in c.items():
+    print(key, val)
+
+def f(x):
+    return x + 123
+
+d = {}
+n = int(input())
+a = int(input())
+nn = 0
+while n != nn:
+    if a in d:
+        print(d[a])
+    elif a not in d:
+        d[a] = f(a)
+        print(d[a])
+    nn += 1
+    if nn == n:
+        break
+    a = int(input())
